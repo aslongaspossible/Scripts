4e47a9819974d4aebd8cae6b0a098beb9b8b57a3
Luddite <dingsf6@gmail.com>
2017-12-19 21:15:34 +0800

    add the simulator

---
+++ stockSimulate.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 19 20:40:30 2017
+
+@author: Luddite
+"""
+
+class stockSimulate:
+    def __init__(self,balance,fee=0.0005):
+        self.balance=balance;
+        self.fee=fee;
+        self.
