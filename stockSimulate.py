30f4721dc025503b13dcf475d344c7e3545f91f4
Luddite <dingsf6@gmail.com>
2017-12-20 17:32:27 +0800

    add method buy in simulate

--- stockSimulate.py
+++ stockSimulate.py
@@ -4,9 +4,12 @@ Created on Tue Dec 19 20:40:30 2017
 
 @author: Luddite
 """
+import time
 
 class stockSimulate:
     def __init__(self,balance,fee=0.0005):
-        self.balance=balance;
-        self.fee=fee;
-        self.
+        self.balance=balance
+        self.fee=fee
+        self.stockList={}
+        
+    def buy(self,code,shares,)
