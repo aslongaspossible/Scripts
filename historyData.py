dabfa5d94e5f61b30dce215b5d38422785e437b1
Luddite <dingsf6@gmail.com>
2017-12-23 17:04:19 +0800

    more information in candlestick picture

--- historyData.py
+++ historyData.py
@@ -23,21 +23,32 @@ class historyData:
         myDate=datetime.strptime(myString, '%Y-%m-%d');
         return dates.date2num(myDate);
      
-    def plotCandleStick(self):
-        fig, ax = plt.subplots()
+    def plotCandleStick(self,height=7,width=10,ma5=False,ma10=False,ma20=False,volume=False):
+        times=self.data.index.map(self.stringToFloatDate)
+        if(volume):
+            fig,(ax,ax2)=plt.subplots(nrows=2,sharex='col')
+            ax2.bar(times,self.data.volume)
+        else:
+            fig, ax = plt.subplots()
         fig.subplots_adjust(bottom=0.2)
-        fig.set_figheight(7)
-        fig.set_figwidth(15)
+        fig.set_figheight(height)
+        fig.set_figwidth(width)
         ax.xaxis.set_major_formatter(dates.DateFormatter('%Y-%m-%d'))
         plt.xticks(rotation=45)
-        candlestick_ohlc(ax,self.ohlc,width =0.4,colorup='#77d879',colordown='#db3f3f')
+        if(ma5):
+            ax.plot(times,self.data.ma5)
+        if(ma10):
+            ax.plot(times,self.data.ma10)
+        if(ma20):
+            ax.plot(times,self.data.ma20)
+        candlestick_ohlc(ax,self.ohlc,width =0.4,colorup='r',colordown='g')
         fig.savefig(self.code+'_'+self.start+'_'+self.end+'.png')
         
-    def plotCloseLine(self):
+    def plotCloseLine(self,height=7,width=10):
         fig, ax = plt.subplots()
         fig.subplots_adjust(bottom=0.2)
-        fig.set_figheight(7)
-        fig.set_figwidth(15)
+        fig.set_figheight(height)
+        fig.set_figwidth(width)
         ax.xaxis.set_major_formatter(dates.DateFormatter('%Y-%m-%d'))
         plt.xticks(rotation=45)
         plt.plot(self.data.index.map(self.stringToFloatDate), self.data.close)
